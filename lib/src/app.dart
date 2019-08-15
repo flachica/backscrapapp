d09f36516ea105f1854555fd01ef0ae58dca7172
Fernando La Chica <fernandolachica@gmail.com>
2019-08-15 20:24:00 +0200

    [Added] Añado el track para Analytics

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,7 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:backscrapapp/src/ui/navigations/router.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  AppState createState() => new AppState();
+}
+
+class AppState extends State<App> {
+  FirebaseMessaging _firebaseMessaging = new FirebaseMessaging();
+
+  @override
+  void initState() {
+    print('Inicialización');
+    super.initState();
+
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) {
+        print('on message $message');
+        return;
+      },
+      onResume: (Map<String, dynamic> message) {
+        print('on resume $message');
+        return;
+      },
+      onLaunch: (Map<String, dynamic> message) {
+        print('on launch $message');
+        return;
+      },
+    );
+
+    _firebaseMessaging.getToken().then((token){
+      print(token);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(

4a0c2446ef2d71835adad11839987610e70f6a8d
Fernando La Chica <fernandolachica@gmail.com>
2019-08-16 17:37:04 +0200

    [fix] No se ejecuta el main directamente, se ejecutará la clase que tiene la configuración correcta

--- lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:backscrapapp/src/app.dart';
-import 'package:backscrapapp/src/resources/env.dart';
-import 'package:backscrapapp/config/api_from_remote.dart';
-
-Env env = ApiFromRemote();
-
-void main() => runApp(App(env: env,));
